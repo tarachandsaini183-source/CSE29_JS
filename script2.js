423b9b7cd8a052524401a5a030d18c170be36a4f
Sharad Saini <tarachandsaini183@gmail.com>
2026-02-25 21:42:29 +0530

    commit

---
+++ script2.js
@@ -0,0 +1,8 @@
+// refrence data type (array, fonction, object)
+//symbol data type 
+const sysm1=Symbol(4)
+const sysm2=Symbol(4)
+console.log(sysm1);
+console.log(sysm2);
+console.log(sysm1 == sysm2);
+console.log(sysm1 === sysm2);
