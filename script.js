d7b9c7cc5edf00c79caa8b7d5ffa18f3d0d94a15
Sharad Saini <tarachandsaini183@gmail.com>
2026-02-25 19:02:23 +0530

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+// document.write ("Hello World 1");
+// primitive data types(number, string, boolean, null, undefined)
+var num;
+console.log(num);
+console.log(typeof(num));
+num = false
+console.log(num);
+console.log(typeof(num));
+num = "10";
+console.log(num);
+console.log(typeof(num));
+num = 10;
+console.log(num);
+console.log(typeof(num));
+var num = null;
+console.log(num);
+console.log(typeof(num));
